b97e23896b514b1f1f13f175a3bedc35481fe4cc
chilensis <chilensis.dev@gmail.com>
2025-08-06 16:20:52 +0900

    Update Status code

--- src/response.ts
+++ src/response.ts
@@ -2,7 +2,7 @@ import { APIGatewayProxyResultV2 } from "aws-lambda";
 
 export const response = (url: string) : APIGatewayProxyResultV2 => {
     return {
-        statusCode: 301,
+        statusCode: 302,
         headers: {
             Location: url,
         }
