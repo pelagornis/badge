8e28b988ea839a3d9aaccecbd1bb57149aa89f29
chilensis <chilensis.dev@gmail.com>
2025-08-06 22:00:06 +0900

    Update index

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { response } from "./response";
 
 export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
   console.log("Event:", JSON.stringify(event));
-
+  console.log("Path:", event.rawPath);
   const path = event.rawPath;
   const url = redirectURL(path);
   return response(url);
